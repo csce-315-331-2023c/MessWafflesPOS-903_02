30c9392b8679f04efead1d5a8ac50381a09f082d
Will Taylor <123038187+willtaylor03@users.noreply.github.com>
2023-10-31 17:54:52 -0500

    rearranged functions, added delete function for menu items and ingredients to manager backend

--- backend/routes/manager.js
+++ backend/routes/manager.js
@@ -14,18 +14,6 @@ router.get('/items', async (req, res) => {
     }
 });
 
-router.post('/inventory', async(req,res) => {
-    const{item,quantity} = req.body
-    try{
-        await db.query('INSERT INTO inventory (item, quantity) VALUES ($1, $2) ON CONFLICT (item) DO UPDATE SET quantity = $2',[item,quantity])
-        res.status(201).send(`Added ingredient ${item}`)
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).send("Server error");
-    }
-});
-
 router.post('/items', async(req,res) => {
     const{item,price,ingredients,category} = req.body
     try{
@@ -38,9 +26,44 @@ router.post('/items', async(req,res) => {
     }
 });
 
+router.delete('/items', async(req,res) => {
+    const{item} = req.body
+    try{
+        await db.query('DELETE FROM items WHERE item = $1',[item])
+        res.status(201).send(`Deleted item ${item}`)
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
+
 router.get('/inventory', async (req, res) => {
     const result = await db.query("SELECT * FROM inventory ORDER BY item");
     res.send(result);
 });
 
+router.post('/inventory', async(req,res) => {
+    const{item,quantity} = req.body
+    try{
+        await db.query('INSERT INTO inventory (item, quantity) VALUES ($1, $2) ON CONFLICT (item) DO UPDATE SET quantity = $2',[item,quantity])
+        res.status(201).send(`Added ingredient ${item}`)
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
+
+router.delete('/inventory', async(req,res) => {
+    const{item} = req.body
+    try{
+        await db.query('DELETE FROM inventory WHERE item = $1',[item])
+        res.status(201).send(`Deleted ingredient ${item}`)
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
 export default router;
