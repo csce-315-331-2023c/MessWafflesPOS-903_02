6b2a052aa94c38c54beed16bb9e2a527472ff8c8
Will Taylor <123038187+willtaylor03@users.noreply.github.com>
2023-10-31 17:29:42 -0500

    added function to update/add menu items

--- backend/routes/manager.js
+++ backend/routes/manager.js
@@ -6,7 +6,7 @@ const router = new Router();
 // example route
 router.get('/items', async (req, res) => {
     try {
-        const result = await db.query("SELECT * FROM items");
+        const result = await db.query("SELECT * FROM items ORDER BY item");
         res.send(result);
     } catch (err) {
         console.error(err);
@@ -17,7 +17,7 @@ router.get('/items', async (req, res) => {
 router.post('/inventory', async(req,res) => {
     const{item,quantity} = req.body
     try{
-        await db.query('INSERT INTO inventory (item, quantity) VALUES ($1, $2)',[item,quantity])
+        await db.query('INSERT INTO inventory (item, quantity) VALUES ($1, $2) ON CONFLICT (item) DO UPDATE SET quantity = $2',[item,quantity])
         res.status(201).send(`Added ingredient ${item}`)
     }
     catch (err) {
@@ -26,8 +26,20 @@ router.post('/inventory', async(req,res) => {
     }
 });
 
+router.post('/items', async(req,res) => {
+    const{item,price,ingredients,category} = req.body
+    try{
+        await db.query('INSERT INTO items (item, price, ingredients, category) VALUES ($1, $2, $3, $4) ON CONFLICT (item) DO UPDATE SET price = $2, ingredients = $3, category = $4',[item,price,ingredients,category])
+        res.status(201).send(`Added item ${item}`)
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
+
 router.get('/inventory', async (req, res) => {
-    const result = await db.query("SELECT * FROM inventory");
+    const result = await db.query("SELECT * FROM inventory ORDER BY item");
     res.send(result);
 });
 
