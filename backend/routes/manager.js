c443fb8a411eec79b6e5f6301dcb76008082cf18
Will Taylor <123038187+willtaylor03@users.noreply.github.com>
2023-10-31 13:09:01 -0500

    created initial manager backend queries

--- backend/routes/manager.js
+++ backend/routes/manager.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import pool from '../db/connection.js';
 const router = express.Router();
 
 // example route
 router.get('/items', (req, res) => {
-    res.send('Get all items');
+    pool.query("SELECT * FROM items", function (err, result){
+        if(err) throw err;
+        res.send(result);
+    });
+});
+
+router.get('/inventory', (req, res) => {
+    pool.query("SELECT * FROM inventory", function (err, result){
+        if(err) throw err;
+        res.send(result);
+    });
 });
 
 export default router;
