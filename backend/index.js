3965e502f1f60fe7875bc46eab409a7f4dff2227
Hemanth <hmukesh@outlook.com>
2023-10-30 19:52:47 -0500

    added routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 // backend code here
 const express = require('express');
+      cashier = require('./routes/cashier');
+      manager = require('./routes/manager');
 const app = express();
+const port = 3000;
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000')
-    console.log('Hello World!')
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/user', (req, res) => {
+    res.send('Hi User!')
+});
+
+app.use('/cashier', cashier);
+app.use('/manager', manager);
+
+app.listen(port, () => {
+    console.log('Server listening at http://localhost:${port}');
 });
