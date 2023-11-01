7a960171484b4b8d158f548056f619d8aec54e52
amurthy729 <79883972+amurthy729@users.noreply.github.com>
2023-10-31 19:39:49 -0500

    changed backend to listen on port 5000; frontend having issues with CORS auth to get list of items

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import express from 'express';
 import { pool } from './db/connection.js';
 import mountRoutes from './routes/routes.js';
 
-const port = 3000;
+const port = 5000;
 
 // create express app and mount routes
 const app = express();
--- frontend/mess-pos/src/components/Cashier.js
+++ frontend/mess-pos/src/components/Cashier.js
@@ -1,26 +1,21 @@
 import React, {useEffect, useState} from 'react'
 import "./Cashier.css"
 import Button from 'react-bootstrap/Button'
+import axios from 'axios'
 
 const Cashier = () => {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        async function getItems() {
-            const response = await fetch(`http://localhost:5000/cashier`);
-            if(!response.ok){
-                const message = `An error has occured: ${response.statusText}`;
-                window.alert(message);
-            return;
-            }
-        const items = await response.json();
-        setItems(items);
-        }
+        axios.get('http://localhost:5000/manager/items')
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    })
 
-        getItems();
-
-        return;
-    }, [items.length]);
     return (
         <main id='cashierSection'>        
             {/* Cashier has two sections: A checkout section, and a Add items sections, roughly 4:6 or 3:7
@@ -28,11 +23,11 @@ const Cashier = () => {
             Add items section has two subsections: 'categories' and 'items'
             For now, we'll only implement main items */}
             {/* checkout */}
-            <div id= "itemsSection" class="col-4 border">
+            <div id= "itemsSection" className="col-4 border">
                 Checkout
             </div>
             {/* items */}
-            <div id="menuSection" class = "col-8 border">
+            <div id="menuSection" className="col-8 border">
                 Items
             </div>
         </main>
