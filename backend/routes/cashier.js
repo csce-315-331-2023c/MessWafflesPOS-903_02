7add36ae13f941d10d4eae9938b643a5d75d8423
Will Taylor <123038187+willtaylor03@users.noreply.github.com>
2023-11-01 13:44:50 -0500

    added backend function to update order tables when an order is made

--- backend/routes/cashier.js
+++ backend/routes/cashier.js
@@ -20,5 +20,22 @@ router.get('/items', async(req, res) => {
     const result = await db.query("SELECT * FROM items");
     res.send(result);
 })
+router.get('/order', async(req, res) => {
+    const result = await db.query("SELECT * FROM orders ORDER BY order_number DESC LIMIT 30");
+    res.send(result);
+})
 
+router.post('/order', async(req,res) => {
+    const orderNumber = await db.query('SELECT order_number FROM orders ORDER BY order_number DESC LIMIT 1')
+    const orderNum = orderNumber.rows[0].order_number + 1
+    const{item,order_date,total_price} = req.body
+    try{
+        await db.query('INSERT INTO orders (order_number, item, order_date, total_price) VALUES ($1, $2, $3, $4)',[orderNum,item,order_date,total_price])
+        res.status(201).send(`Added order ${orderNum}`)
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
 export default router;
