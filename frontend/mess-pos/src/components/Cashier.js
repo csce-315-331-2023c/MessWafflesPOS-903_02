d7b63695ddacc461baa7f1bc39b4aacafe966f94
amurthy729 <79883972+amurthy729@users.noreply.github.com>
2023-10-31 19:06:18 -0500

    working on api requests for cashier

--- frontend/mess-pos/src/components/Cashier.js
+++ frontend/mess-pos/src/components/Cashier.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import "./Cashier.css"
 import Button from 'react-bootstrap/Button'
 
 const Cashier = () => {
-    function getItems() {
+    const [items, setItems] = useState([]);
 
-    }
+    useEffect(() => {
+        async function getItems() {
+            const response = await fetch(`http://localhost:5000/cashier`);
+            if(!response.ok){
+                const message = `An error has occured: ${response.statusText}`;
+                window.alert(message);
+            return;
+            }
+        const items = await response.json();
+        setItems(items);
+        }
+
+        getItems();
+
+        return;
+    }, [items.length]);
     return (
         <main id='cashierSection'>        
             {/* Cashier has two sections: A checkout section, and a Add items sections, roughly 4:6 or 3:7
@@ -19,9 +34,6 @@ const Cashier = () => {
             {/* items */}
             <div id="menuSection" class = "col-8 border">
                 Items
-                <Button variant="primary" size="lg">
-          Large button
-        </Button>{' '}
             </div>
         </main>
     )
