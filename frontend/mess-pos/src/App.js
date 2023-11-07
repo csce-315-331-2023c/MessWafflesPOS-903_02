b7cd7f3dac05b60f657dcf43d958b5056b3fbbae
amurthy729 <79883972+amurthy729@users.noreply.github.com>
2023-11-06 18:50:10 -0600

    added basic routing

--- frontend/mess-pos/src/App.js
+++ frontend/mess-pos/src/App.js
@@ -3,18 +3,22 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Cashier from "./components/Cashier"
-import Layout from "./components/Layout"
 import Home from "./components/Home"
+import Manager from "./components/Manager"
 
 const App = () => {
     return(
+        <>
         <BrowserRouter>
+            <Header />
             <Routes>
-                <Route path="/" element={<Layout />} />
-                    <Route index element={<Home />} />
-                    <Route path="cashier" element={<Cashier />} />
+                <Route index element={<Home />} />
+                <Route path="cashier" element={<Cashier />}/>
+                <Route path="manager" element={<Manager />}></Route>
             </Routes>
+            <Footer />
         </BrowserRouter>
+        </>
         // <>
         // <Header />
         // <Cashier />
--- frontend/mess-pos/src/components/Header.js
+++ frontend/mess-pos/src/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./Header.css"
+import {Outlet, Link} from "react-router-dom"
 
 const Header = () => {
   return (
-    <header><h1>Mess Waffles</h1></header>
+    <>
+      <header>
+        <h1>Mess Waffles</h1>
+        <nav>
+          <h2><Link to="/">Home</Link></h2>
+          <h2><Link to="/cashier">Cashier</Link></h2>
+        </nav>
+      </header>
+      <Outlet />
+    </>
+
   )
 }
 
