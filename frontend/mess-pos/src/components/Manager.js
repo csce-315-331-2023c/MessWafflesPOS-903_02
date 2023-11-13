ebdfa79e6fe78ad42384ced1539bc11727e7adcb
amurthy729 <79883972+amurthy729@users.noreply.github.com>
2023-11-13 16:19:01 -0600

    basic layout will all tabs for manager page

--- frontend/mess-pos/src/components/Manager.js
+++ frontend/mess-pos/src/components/Manager.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
 
 const Manager = () => {
   return (
-    <div>Manager</div>
+    <main>
+    <Tabs defaultActiveKey="inventory">
+      <Tab eventKey="inventory" title="Inventory">
+        {/* Content for inventory tab */}
+        Inventory
+      </Tab>
+      <Tab eventKey="menu" title="Menu">
+        {/* Content for Menu tab */}
+        Menu
+      </Tab>
+      <Tab eventKey="usage" title="Usage">
+        {/* Product Usage */}
+        Product Usage
+      </Tab>
+      <Tab eventKey="sales" title="Sales">
+        {/* Sales Report */}
+        Sales
+      </Tab>
+      <Tab eventKey="excess" title="Excess">
+        {/* Excess Report */}
+        Excess Report
+      </Tab>
+      <Tab eventKey="restock" title="Restock">
+        {/* Restock Report*/}
+        Restock
+      </Tab>
+      <Tab eventKey="trends" title="Trends">
+        {/* Ordering Trends (What sells together) */}
+        Ordering Trends
+      </Tab>
+    </Tabs>
+    </main>
   )
 }
 
