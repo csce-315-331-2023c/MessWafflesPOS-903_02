d26131ff817b1d14ab2ffcc8f15b64a6824fb403
Will Taylor <123038187+willtaylor03@users.noreply.github.com>
2023-11-13 08:46:24 -0600

    added initial items page on manager frontend

--- frontend/mess-pos/src/components/Manager.js
+++ frontend/mess-pos/src/components/Manager.js
@@ -22,9 +22,27 @@ const Manager = () => {
     ingsList.push(JSON.stringify(ings.rows[i].item).substring(1,JSON.stringify(ings.rows[i].item).length-1))
     quantList.push(ings.rows[i].quantity)
   }
-
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/manager/items')
+        .then(response => {
+            setItems(response.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+  },[]);
+  var itemList = []
+  var priceList = []
+  var ingList = []
+  var catList = []
+  for(let i = 0; i < items.rowCount;i++){
+    itemList.push(JSON.stringify(items.rows[i].item).substring(1,JSON.stringify(items.rows[i].item).length-1))
+    priceList.push(items.rows[i].price)
+    ingList.push(items.rows[i].ingredients)
+    catList.push(JSON.stringify(items.rows[i].category).substring(1,JSON.stringify(items.rows[i].category).length-1))
+  }
   function Inventory() {
-    
     return (
         <>
         <div className= "checkout" >
@@ -47,37 +65,75 @@ const Manager = () => {
     
         </>
     )
-    }
-    function updateInventory(e){
-      e.preventDefault()
-      const form = e.target
-      const formData = new FormData(form);
-      const postdata = Object.fromEntries(formData.entries());
-      console.log(postdata)
-      axios.post('http://localhost:5000/manager/inventory', postdata)
-          .then(response => {
-              console.log(response.data);
-          })
-          .catch(err => {
-              console.log(err);
-          });
-    }
-    function deleteItem(e){
-      e.preventDefault()
-      const form = e.target
-      const formData = new FormData(form);
-      const postdata = Object.fromEntries(formData.entries());
-      console.log(postdata)
-      axios.delete(`http://localhost:5000/manager/inventory/`,{
-        data: postdata
-      })
-          .then(response => {
-              console.log(response.data);
-          })
-          .catch(err => {
-              console.log(err);
-          });
-    }
+  }
+  function updateInventory(e){
+    e.preventDefault()
+    const form = e.target
+    const formData = new FormData(form);
+    const postdata = Object.fromEntries(formData.entries());
+    console.log(postdata)
+    axios.post('http://localhost:5000/manager/inventory', postdata)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+  }
+  function deleteItem(e){
+    e.preventDefault()
+    const form = e.target
+    const formData = new FormData(form);
+    const postdata = Object.fromEntries(formData.entries());
+    console.log(postdata)
+    axios.delete(`http://localhost:5000/manager/inventory/`,{
+      data: postdata
+    })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+  }
+  function Items(){
+    return (
+      <>
+      <div className= "checkout" >
+          <div id = "itemCol" className="itemsCol">Item
+              <ul style={{ display: 'block' }}>
+                  {itemList.map((item, index) => (
+                  <li key={index}>{item}</li>
+                  ))}
+              </ul>
+          </div>
+          
+          <div id = "priceCol" className="itemsCol">Price
+              <ul style={{display: 'block'}}>
+                  {priceList.map((quantity, index) => (
+                  <li key={index} >{quantity}</li>
+                  ))}
+              </ul>
+          </div>
+          <div id = "ingCol" className="itemsCol">Ingredients
+              <ul style={{display: 'block'}}>
+                  {ingList.map((quantity, index) => (
+                  <li key={index} >{quantity}</li>
+                  ))}
+              </ul>
+          </div>
+          <div id = "catCol" className="itemsCol">Category
+              <ul style={{display: 'block'}}>
+                  {catList.map((quantity, index) => (
+                  <li key={index} >{quantity}</li>
+                  ))}
+              </ul>
+          </div>
+      </div>
+  
+      </>
+  )
+  }
   return (
     <>
     <Tabs defaultActiveKey="inventory">
@@ -99,7 +155,9 @@ const Manager = () => {
         </center>
         </div>
       </Tab>
-      <Tab eventKey="menu" title="Menu">Menu</Tab>
+      <Tab eventKey="menu" title="Menu">
+        <Items />
+      </Tab>
     </Tabs>
     </>
   )
