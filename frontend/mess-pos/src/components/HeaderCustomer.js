265ac685dfab1fd872e8b3dedafdea3cb2953a60
kgavvala18 <kgavvala@gmail.com>
2023-11-14 19:56:16 -0600

    added customer page

---
+++ frontend/mess-pos/src/components/HeaderCustomer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./HeaderCustomer.css"
+
+const Header = () => {
+  return (
+    <header><h1>Mess Waffles Customer</h1></header>
+    
+  )
+}
+
+export default Header;
