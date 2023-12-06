b7acf2ed34e129dca9856284ef1eda7392734c9d
kgavvala18 <kgavvala@gmail.com>
2023-12-06 13:54:28 -0600

    fixed customer offcanv

--- frontend/mess-pos/src/components/Customer.js
+++ frontend/mess-pos/src/components/Customer.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Customer.css"
 import Button from 'react-bootstrap/Button'
 import axios from 'axios'
@@ -24,11 +24,11 @@ const Customer = () => {
             .catch(err => {
                 console.log(err);
             })
-    },[]);
+    }, []);
     var entrees = [];
     var drinks = [];
     var seasonal = [];
-    var cold  = [];
+    var cold = [];
     var hot = [];
     const [Item_names, setItemNames] = useState([]);
     const [Prices, setPrices] = useState([]);
@@ -39,10 +39,10 @@ const Customer = () => {
 
         setItemNames(olary => [...olary, name]);
         setPrices(olary => [...olary, price]);
-        setTotal(oldPrice => price_tot+ parseFloat(price));
+        setTotal(oldPrice => price_tot + parseFloat(price));
     }
 
-    function resetItems(){
+    function resetItems() {
         setItemNames(olary => []);
         setPrices(olary => []);
         setTotal(oldPrice => 0.00);
@@ -51,210 +51,210 @@ const Customer = () => {
     function Items() {
 
         function removeOrderItem(i) {
-            if (window.confirm('delete ' + i + ' from the order?')){
-                var array_i = [...Item_names]; 
-                var array_p = [...Prices]; 
+            if (window.confirm('delete ' + i + ' from the order?')) {
+                var array_i = [...Item_names];
+                var array_p = [...Prices];
                 var index = array_i.indexOf(i);
                 var price = array_p[index];
-                
+
                 if (index !== -1) {
                     array_i.splice(index, 1);
                     setItemNames(olary => [...array_i]);
                     array_p.splice(index, 1);
                     setPrices(olary => [...array_p]);
-                    setTotal(oldPrice => price_tot- parseFloat(price));
+                    setTotal(oldPrice => price_tot - parseFloat(price));
                 }
             }
-            
+
 
         };
-        
+
         return (
             <>
-            <div style={{overflowY: "auto"}} className= "card-text" >
-                <div id = "priceCol" className="itemsCol">Item
-                    <ListGroup style={{ display: 'block' }}>
-                        {Item_names.map((item, index) => (
-                        <ListGroupItem  action onClick= { () => {removeOrderItem(item);}} key={index}>{item}</ListGroupItem>
-                        ))}
-                    </ListGroup>
+                <div style={{ overflowY: "auto" }} className="checkout" >
+                    <div id="priceCol" className="itemsCol">Item
+                        <ListGroup style={{ display: 'block' }}>
+                            {Item_names.map((item, index) => (
+                                <ListGroupItem action onClick={() => { removeOrderItem(item); }} key={index}>{item}</ListGroupItem>
+                            ))}
+                        </ListGroup>
+                    </div>
+
+                    <div id="priceCol" className="itemsCol">Price
+                        <ListGroup style={{ display: 'block' }}>
+                            {Prices.map((item, index) => (
+                                <ListGroupItem key={index} >{item}</ListGroupItem>
+                            ))}
+                        </ListGroup>
+                    </div>
                 </div>
-                
-                <div id = "priceCol" className="itemsCol">Price
-                    <ListGroup style={{display: 'block'}}>
-                        {Prices.map((item, index) => (
-                        <ListGroupItem key={index} >{item}</ListGroupItem>
-                        ))}
-                    </ListGroup>
+                <div id="orderInfo" className='card-text'>
+                    Total: ${price_tot.toFixed(2)}
                 </div>
-            </div>
-            <div id="orderInfo" className='card-text'> 
-                Total: ${price_tot.toFixed(2)}
-            </div>
-                
+
             </>
         )
     }
 
-   
 
-    function itemCard(item, price, index,description,picture) {
+
+    function itemCard(item, price, index, description, picture) {
         return (
             <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={picture} />
-            <Card.Body className='card-body'>
-                <Card.Title>{item}</Card.Title>
-                <Card.Text className='card-text'>
-                {description}
-                </Card.Text>
-                <Button onClick={() => {addOrder(item, price);}} key={index} variant="primary">Add</Button>
-            </Card.Body>
+                <Card.Img variant="top" src={picture} />
+                <Card.Body className='card-body'>
+                    <Card.Title>{item}</Card.Title>
+                    <Card.Text className='card-text'>
+                        {description}
+                    </Card.Text>
+                    <Button onClick={() => { addOrder(item, price); }} key={index} variant="primary">Add</Button>
+                </Card.Body>
             </Card>
         );
     }
 
-    
+
     const orders = new Map();
 
 
-    for(let i = 0; i < items.rowCount; i++){
-        const menuItem = {item: JSON.stringify(items.rows[i].item).substring(1,JSON.stringify(items.rows[i].item).length-1), price: JSON.stringify(items.rows[i].price).substring(1,JSON.stringify(items.rows[i].price).length-1),description: JSON.stringify(items.rows[i].description).substring(1,JSON.stringify(items.rows[i].description).length-1),picture: JSON.stringify(items.rows[i].picture).substring(1,JSON.stringify(items.rows[i].picture).length-1)};
-        if(JSON.stringify(items.rows[i].category) == "\"entree\"") {
+    for (let i = 0; i < items.rowCount; i++) {
+        const menuItem = { item: JSON.stringify(items.rows[i].item).substring(1, JSON.stringify(items.rows[i].item).length - 1), price: JSON.stringify(items.rows[i].price).substring(1, JSON.stringify(items.rows[i].price).length - 1), description: JSON.stringify(items.rows[i].description).substring(1, JSON.stringify(items.rows[i].description).length - 1), picture: JSON.stringify(items.rows[i].picture).substring(1, JSON.stringify(items.rows[i].picture).length - 1) };
+        if (JSON.stringify(items.rows[i].category) == "\"entree\"") {
             entrees.push(menuItem);
         }
-        else if(JSON.stringify(items.rows[i].category) == "\"drinks\"") {
+        else if (JSON.stringify(items.rows[i].category) == "\"drinks\"") {
             drinks.push(menuItem);
         }
-        else if(JSON.stringify(items.rows[i].category) == "\"seasonal\"") {
+        else if (JSON.stringify(items.rows[i].category) == "\"seasonal\"") {
             seasonal.push(menuItem);
         }
-        if(JSON.stringify(items.rows[i].weather_type) == "\"cold\""){
+        if (JSON.stringify(items.rows[i].weather_type) == "\"cold\"") {
             cold.push(menuItem);
         }
-        if(JSON.stringify(items.rows[i].weather_type) == "\"hot\""){
+        if (JSON.stringify(items.rows[i].weather_type) == "\"hot\"") {
             hot.push(menuItem);
         }
     }
     var entreesList = [];
     var drinksList = [];
-    var seasonalList =[];
+    var seasonalList = [];
     var coldWeatherItems = [];
     var hotWeatherItems = [];
 
     function addOrder(name, price) {
         if (orders.has(name)) {
-            orders.set(name, {price: price, quantity: orders.get(name).quantity + 1})
+            orders.set(name, { price: price, quantity: orders.get(name).quantity + 1 })
         }
         else {
-            orders.set(name, {price: price, quantity: 1});
+            orders.set(name, { price: price, quantity: 1 });
         }
-        updateItems(name,price);
+        updateItems(name, price);
         console.log(name, orders.get(name).quantity, orders.get(name).price);
     }
-    entrees.forEach((item, index)=> {
-        entreesList.push( itemCard(item.item, item.price, index,item.description,item.picture))
+    entrees.forEach((item, index) => {
+        entreesList.push(itemCard(item.item, item.price, index, item.description, item.picture))
     })
-    drinks.forEach((item, index)=> {
-        drinksList.push( itemCard(item.item, item.price, index,item.description,item.picture))
+    drinks.forEach((item, index) => {
+        drinksList.push(itemCard(item.item, item.price, index, item.description, item.picture))
     })
-    seasonal.forEach((item, index)=> {
-        seasonalList.push( itemCard(item.item, item.price, index,item.description,item.picture))
+    seasonal.forEach((item, index) => {
+        seasonalList.push(itemCard(item.item, item.price, index, item.description, item.picture))
     })
-    cold.forEach((item, index)=> {
-        coldWeatherItems.push( itemCard(item.item, item.price, index,item.description,item.picture))
+    cold.forEach((item, index) => {
+        coldWeatherItems.push(itemCard(item.item, item.price, index, item.description, item.picture))
     })
-    hot.forEach((item, index)=> {
-        hotWeatherItems.push( itemCard(item.item, item.price, index,item.description,item.picture))
+    hot.forEach((item, index) => {
+        hotWeatherItems.push(itemCard(item.item, item.price, index, item.description, item.picture))
     })
-    
+
     const EntreePage = () => {
-        
+
         return (
-          <Row className='card-body'>
-            {entreesList.map((item, index) => (
-              <Col key={index} sm={3}>
-                {/* Adjust  based on how many items you want in a row */}
-                <ListGroup>
-                  <ListGroup.Item>{item}</ListGroup.Item>
-                </ListGroup>
-              </Col>
-            ))}
-          </Row>
+            <Row className='card-body'>
+                {entreesList.map((item, index) => (
+                    <Col key={index} sm={3}>
+                        {/* Adjust  based on how many items you want in a row */}
+                        <ListGroup>
+                            <ListGroup.Item>{item}</ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                ))}
+            </Row>
         );
     }
 
     const DrinksPage = () => {
-        
+
         return (
-          <Row className='card-body'>
-            {drinksList.map((item, index) => (
-              <Col key={index} sm={3}>
-                {/* Adjust  based on how many items you want in a row */}
-                <ListGroup>
-                  <ListGroup.Item>{item}</ListGroup.Item>
-                </ListGroup>
-              </Col>
-            ))}
-          </Row>
+            <Row className='card-body'>
+                {drinksList.map((item, index) => (
+                    <Col key={index} sm={3}>
+                        {/* Adjust  based on how many items you want in a row */}
+                        <ListGroup>
+                            <ListGroup.Item>{item}</ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                ))}
+            </Row>
         );
     }
 
     const SeasonalPage = () => {
-        
+
         return (
-          <Row className='card-body'>
-            {seasonalList.map((item, index) => (
-              <Col key={index} sm={3}>
-                {/* Adjust  based on how many items you want in a row */}
-                <ListGroup>
-                  <ListGroup.Item>{item}</ListGroup.Item>
-                </ListGroup>
-              </Col>
-            ))}
-          </Row>
+            <Row className='card-body'>
+                {seasonalList.map((item, index) => (
+                    <Col key={index} sm={3}>
+                        {/* Adjust  based on how many items you want in a row */}
+                        <ListGroup>
+                            <ListGroup.Item>{item}</ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                ))}
+            </Row>
         );
     }
 
-   const RecItems  = () =>{
-    const [temp, setTemp] = useState([]);
-    useEffect(() => {
-        axios.get('http://api.weatherapi.com/v1/current.json?Key=f9d76b0584124e86bfa144719232711&q=77840')
-            .then(response => {
-                setTemp(response.data.current.temp_f);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    },[]);
-    if(temp <= 60){
-        return (
-            <Row className='card-body'>
-            {coldWeatherItems.map((item, index) => (
-                <Col key={index} sm={3}>
-                {/* Adjust  based on how many items you want in a row */}
-                <ListGroup>
-                    <ListGroup.Item>{item}</ListGroup.Item>
-                </ListGroup>
-                </Col>
-            ))}
-            </Row>
-        );
+    const RecItems = () => {
+        const [temp, setTemp] = useState([]);
+        useEffect(() => {
+            axios.get('http://api.weatherapi.com/v1/current.json?Key=f9d76b0584124e86bfa144719232711&q=77840')
+                .then(response => {
+                    setTemp(response.data.current.temp_f);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }, []);
+        if (temp <= 60) {
+            return (
+                <Row className='card-body'>
+                    {coldWeatherItems.map((item, index) => (
+                        <Col key={index} sm={3}>
+                            {/* Adjust  based on how many items you want in a row */}
+                            <ListGroup>
+                                <ListGroup.Item>{item}</ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                    ))}
+                </Row>
+            );
+        }
+        else if (temp >= 70) {
+            return (
+                <Row className='card-body'>
+                    {hotWeatherItems.map((item, index) => (
+                        <Col key={index} sm={3}>
+                            {/* Adjust  based on how many items you want in a row */}
+                            <ListGroup>
+                                <ListGroup.Item>{item}</ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                    ))}
+                </Row>
+            );
+        }
     }
-    else if (temp >= 70){
-        return (
-            <Row className='card-body'>
-            {hotWeatherItems.map((item, index) => (
-                <Col key={index} sm={3}>
-                {/* Adjust  based on how many items you want in a row */}
-                <ListGroup>
-                    <ListGroup.Item>{item}</ListGroup.Item>
-                </ListGroup>
-                </Col>
-            ))}
-            </Row>
-        );
-    }
-   }
 
     function Info() {
         const [show, setShow] = useState(false);
@@ -264,36 +264,36 @@ const Customer = () => {
 
         return (
             <>
-            <Button className='SeeOrder' variant="primary" size="lg" onClick={handleShow}>
-                View Order
-            </Button>
+                <Button className='SeeOrder' variant="primary" size="lg" onClick={handleShow}>
+                    View Order
+                </Button>
 
-            <Offcanvas show={show} onHide={handleClose} backdrop = "static">
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Current Order</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body className='card-body'>
-                        <p>Click an item to remove it</p> 
+                <Offcanvas show={show} onHide={handleClose} backdrop="static">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Current Order</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body className='card-body'>
+                        <p>Click an item to remove it</p>
                         <Items />
-                        <div id= 'orderactions'>
-                            <Button onClick={() => {place_order();}} variant="primary" size="lg">Place Order</Button>
-                            <Button  onClick={() => {resetItems();}}variant="primary" size="lg">Cancel Order</Button>
+                        <div id='orderactions'>
+                            <Button onClick={() => { place_order(); }} variant="primary" size="lg">Place Order</Button>
+                            <Button onClick={() => { resetItems(); }} variant="primary" size="lg">Cancel Order</Button>
                         </div>
-                </Offcanvas.Body>
-            </Offcanvas>
+                    </Offcanvas.Body>
+                </Offcanvas>
             </>
         );
     }
 
-    function place_order(){
+    function place_order() {
         const date = new Date();
         var n = 0;
-        for( let i = 0; i < Prices.length; i++){
+        for (let i = 0; i < Prices.length; i++) {
             console.log(Prices[i].replace("\"", ""));
 
             n += parseFloat(Prices[i].replace("\"", ""));
         }
-        
+
 
         console.log(n);
         console.log(Item_names);
@@ -304,7 +304,7 @@ const Customer = () => {
             order_date: date,
             total_price: n
         }
-        
+
         axios.post('https://messwafflespos.onrender.com/api/cashier/order', postdata)
             .then(response => {
                 console.log(response.data);
@@ -313,17 +313,17 @@ const Customer = () => {
                 console.log(err);
             });
         resetItems();
-    }   
+    }
 
 
     return (
-        <main id='cashierSection' className='card-body'>        
+        <main id='cashierSection' className='card-body'>
             {/* Cashier has two sections: A checkout section, and a Add items sections, roughly 4:6 or 3:7
             Checkout sections should take list of items in 'cart' and display with item name, quantity, and total price
             Add items section has two subsections: 'categories' and 'items'
             For now, we'll only implement main items */}
             {/* checkout */}
-            
+
 
             {/* <div  className="col-4 border" >
                 <Items />
@@ -335,28 +335,28 @@ const Customer = () => {
 
                 </div>
             </div> */}
-            <Info/>
+            <Info />
             <div id="menuSection" className='card-body'>
                 <Tabs defaultActiveKey="entree" fill>
                     <Tab eventKey="entree" title="Entrees" >
-                       <div className='card-body'>
-                            <EntreePage/>
-                       </div>
-                
+                        <div className='card-body'>
+                            <EntreePage />
+                        </div>
+
                     </Tab>
                     <Tab eventKey="drink" title="Drinks">
                         <div>
-                            <DrinksPage/>
-                       </div>
+                            <DrinksPage />
+                        </div>
                     </Tab>
                     <Tab eventKey="seasonal" title="Seasonal Items">
                         <div>
-                            <SeasonalPage/>
-                       </div>
+                            <SeasonalPage />
+                        </div>
                     </Tab>
                     <Tab eventKey="rec" title="Recommended Items (based on weather)">
                         <div>
-                            <RecItems/>
+                            <RecItems />
                         </div>
                     </Tab>
                 </Tabs>
