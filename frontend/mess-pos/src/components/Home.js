1d750a4dd8f57b2b71d16563f06a9ff8a581fb1e
Will Taylor <123038187+willtaylor03@users.noreply.github.com>
2023-12-05 19:50:48 -0600

    added alt tags to images for accessibility

--- frontend/mess-pos/src/components/Home.js
+++ frontend/mess-pos/src/components/Home.js
@@ -25,22 +25,22 @@ const Home = () => {
                 <Row>
                     <Col>
                         <div className="row mb-4">
-                            <Image src="https://cloudfront-us-east-1.images.arcpublishing.com/gray/MCO6C6ALLNMLDDBJHB7GV4OQW4.jpg" fluid /> 
+                            <Image src="https://cloudfront-us-east-1.images.arcpublishing.com/gray/MCO6C6ALLNMLDDBJHB7GV4OQW4.jpg" fluid alt="Picture of the Mess exterior" /> 
                         </div>
                         
                         <div>
-                            <Image src="https://images.squarespace-cdn.com/content/v1/5ad69812da02bc49e7489bbe/1680466719039-EFPFIL4K6HJKMUOB13N2/IMG_3038-1.jpg" fluid/>
+                            <Image src="https://images.squarespace-cdn.com/content/v1/5ad69812da02bc49e7489bbe/1680466719039-EFPFIL4K6HJKMUOB13N2/IMG_3038-1.jpg" fluid alt="Waffles image"/>
                         </div>
                         
                     </Col>
                     <Col>
                         <p className="lead">Mess waffles is a creative comfort food destination in College Station, texas. It was started by a former Texas A&M student as a food truck in 2014, and after much growth moved into a restaurant location in College Station’s Century Square. </p>
                         <Row> 
-                            <Image src="https://images.squarespace-cdn.com/content/v1/5ad69812da02bc49e7489bbe/1590347742303-O632SK8DX7EQMFLP0INS/MessWaffles05-4.jpg" fluid />
+                            <Image src="https://images.squarespace-cdn.com/content/v1/5ad69812da02bc49e7489bbe/1590347742303-O632SK8DX7EQMFLP0INS/MessWaffles05-4.jpg" fluid alt ="Mess chicken and waffles"/>
                             
                         </Row>
                         <div className="row mt-4">
-                            <Image src="https://insitebrazosvalley.com/downloads/32303/download/unnamed-4.jpg?cb=57605109cf3f4d4dface7aef63f3e140&w=1200" fluid />
+                            <Image src="https://insitebrazosvalley.com/downloads/32303/download/unnamed-4.jpg?cb=57605109cf3f4d4dface7aef63f3e140&w=1200" fluid alt="a variety of mess food"/>
                         </div>
                         <div id="weatherapi-weather-widget-3">
                             <Script/>
