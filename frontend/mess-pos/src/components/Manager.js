9b8f9ba83b1ad13261ec19f33487d7e6e8ada898
Hemanth <hmukesh@outlook.com>
2023-12-05 21:20:47 -0600

    implemented employees tab

--- frontend/mess-pos/src/components/Manager.js
+++ frontend/mess-pos/src/components/Manager.js
@@ -43,6 +43,10 @@ const Manager = () => {
   // Trends
   const trendsMap = new Map();
   const trendsPairs = [];
+  // Employees
+  const [employeeData, setEmployeeData] = useState([]);
+  const [loadingEmployees, setLoadingEmployees] = useState(true);
+  const [triggerEmployeeData, setTriggerEmployeeData] = useState(false);
 
   // useEffect (on site render)
   useEffect(() => {
@@ -60,7 +64,20 @@ const Manager = () => {
       .catch(error => {
         console.log(error);
       })
+    setTriggerEmployeeData(!triggerEmployeeData);
   },[]);
+
+  useEffect(() => {
+    axios
+        .get("http://localhost:5000/auth/employees")
+        .then((response) => {
+            setEmployeeData(response.data);
+            setLoadingEmployees(false);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }, [triggerEmployeeData]);
 
   // Form Submissions
   // Usage Report
@@ -485,6 +502,47 @@ const Manager = () => {
         
   }
 
+  // employee code
+  function updateEmployee(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const postdata = Object.fromEntries(formData.entries());
+    console.log(postdata);
+    axios
+        .post("http://localhost:5000/auth/employees", {
+            id: postdata.ID,
+            name: postdata.Name,
+            email: postdata.Email,
+            role: postdata.Role,
+        })
+        .then((response) => {
+            console.log(response.data);
+            setTriggerEmployeeData(!triggerEmployeeData);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+  }
+  function deleteEmployee(e) {
+      e.preventDefault();
+      const form = e.target;
+      const formData = new FormData(form);
+      const postdata = Object.fromEntries(formData.entries());
+      console.log(postdata);
+      axios
+          .delete("http://localhost:5000/auth/employees", {
+              data: { id: postdata.ID },
+          })
+          .then((response) => {
+              console.log(response.data);
+              setTriggerEmployeeData(!triggerEmployeeData);
+          })
+          .catch((err) => {
+              console.log(err);
+          });
+  }
+
   // Driver Code (basically, all this code is going to be used for is to check for loading)
   if(!loadingUsage) {
     processIngredients();
@@ -683,6 +741,80 @@ const Manager = () => {
           </tbody>
         </Table>
       </Tab>
+      
+      <Tab eventKey="employees" title="Employees">
+          {loadingEmployees ? (
+              <p>Loading Employees...</p>
+          ) : (
+              <>
+                  <Table striped bordered hover>
+                      <thead>
+                          <tr>
+                              <th>ID</th>
+                              <th>Name</th>
+                              <th>Role</th>
+                              <th>Email</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          {employeeData.rows.map((val, key) => {
+                              return (
+                                  <tr key={key}>
+                                      <td>{val.id}</td>
+                                      <td>{val.name}</td>
+                                      <td>{val.role}</td>
+                                      <td>{val.email}</td>
+                                  </tr>
+                              );
+                          })}
+                      </tbody>
+                  </Table>
+                  <center>
+                      <form onSubmit={updateEmployee}>
+                          Update/Create Employee:
+                          <input
+                              name="ID"
+                              label="ID"
+                              placeholder="ID"
+                          />
+                          <input
+                              name="Name"
+                              label="Name"
+                              placeholder="Name"
+                          />
+                          <select
+                              name="Role"
+                              label="Role"
+                              style={{
+                                  width: "200px",
+                                  height: "30px",
+                              }}
+                          >
+                              <option value="Manager">Manager</option>
+                              <option value="Cashier">Cashier</option>
+                          </select>
+                          <input
+                              name="Email"
+                              label="Email"
+                              placeholder="Email"
+                          />
+                          <Button type="submit">Submit</Button>
+                      </form>
+                      <br></br>
+                      <form onSubmit={deleteEmployee}>
+                          Delete Employee:
+                          <input
+                              name="ID"
+                              label="ID"
+                              placeholder="ID"
+                          />
+                          <Button type="submit">Submit</Button>
+                      </form>
+                  </center>
+              </>
+          )}
+      </Tab>
+
     </Tabs>
     </main>
   )
